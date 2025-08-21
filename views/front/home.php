5391b3be03ab3d07a61c1292bf37ff66639b1626
Çınar Özgürgil <cozgurgil@gmail.com>
2025-08-21 16:35:41 +0300

    lalemin-resim-güncellemesş

--- views/front/home.php
+++ views/front/home.php
@@ -16,18 +16,38 @@
 
     <div class="main">
         <div class="head">
-            <div class="row">
-                <div class="col-lg-3">Hakkımızda</div>
-                <div class="col-lg-3">Anasayfa</div>
-                <div class="col-lg-6">İletişim</div>
-            </div>
+                <nav class="navbar navbar-expand-lg ">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">Navbar</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                                </li>
+                            </ul>
+                            <form class="d-flex" role="search">
+                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                                <button class="btn btn-outline-success" type="submit">Search</button>
+                            </form>
+                        </div>
+                    </div>
+                </nav>
         </div>
         <div class="main-body">
             <div class="container">
                 <div class="row">
                     <div class="d-flex align-items-center">
                         <div class="col-4">
-                            <img src="img/image.png" alt="" width="100%">
+                            <img src="img/image.png" alt="" width="120%">
                         </div>
                         <div class="col-8">
                             <div class="text-center">
